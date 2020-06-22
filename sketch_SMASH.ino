d2a14e7905ca74760442881794d607a310e27393
Lars Møen Haukland <Lars.haukland@gmail.com>
2020-06-23 00:08:17 +0200

    update smash.io
    removed lcd screen from lock, added buzzer. This is my code as I mess around and make the piano note c4 to e4 go on all the number of the keypad. 
    table of notes in Hz = https://pages.mtu.edu/~suits/notefreqs.html

--- sketch_SMASH.ino
+++ sketch_SMASH.ino
@@ -17,90 +17,111 @@ byte colPins[COLS] = {5,7,3};
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
-#include <LiquidCrystal.h>
-
-LiquidCrystal lcd(13,12,11,10,9,8);
-
 int servoPin = 0;
 Servo servo1;
 
 int c = 0;
 
-char code[]= {'1','2','3','4'};
-short a=0,i=0,s=0,j=0;
+char code[]= {'0','0','0','0'};
+short a=0,i=0,s=0,j=0,n=120;
 
-int greenPin = A0;
-int redPin = A1;
+const int buzzer = A5;
 
 
 
 void setup() {
   // put your setup code here, to run once:
-  lcd.begin(16,2);
-
+  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
   servo1.attach(servoPin);
-  servo1.write(-10000);
+  //servo1.write(-10000);
   delay(1000);
-  servo1.write(80);
+  servo1.write(40);
   
-  lcd.print("Standby");
-
-  pinMode(greenPin, OUTPUT);
-  pinMode(redPin, OUTPUT);
  
 
 }
 
 void GetCode(){                  //Getting code sequence
+       n=120;
        i=0;                      //All variables set to 0
        a=0;
        j=0; 
        char key = keypad.getKey();                              
      while(key != '#'){                                     //The user press # to confirm the code otherwise he can keep typing
            key = keypad.getKey();                         
-             if(key != NO_KEY && key != '#' ){       //If the char typed isn't # and neither "nothing"
-              lcd.setCursor(j,1);                                  //This to write "*" on the LCD whenever a key is pressed it's position is controlled by j
-              lcd.print("*");
-              j++;
+             if(key != NO_KEY && key != '#' ){       //If the char typed isn't # and neither "nothing"                              
+              //tone(buzzer, n, 500);
+              //delay(500);
+            if(key == '1'){
+                  tone(buzzer, 0.26163 * 1000, 200);
+                 }else if(key == '2'){
+                  tone(buzzer, 0.29366 * 1000, 200);
+                 }else if(key == '3'){
+                  tone(buzzer, 0.32963 * 1000, 200);
+                 }else if(key == '4'){
+                  tone(buzzer, 0.34923 * 1000, 200);
+                 }else if(key == '5'){
+                  tone(buzzer, 0.392 * 1000, 200);
+                 }else if(key == '6'){
+                  tone(buzzer, 0.44 * 1000, 200);
+                 }else if(key == '7'){
+                  tone(buzzer, 0.49388 * 1000, 200);
+                 }else if(key == '8'){
+                  tone(buzzer, 0.52325 * 1000, 200);
+                 }else if(key == '9'){
+                  tone(buzzer, 0.58733 * 1000, 200);
+                 }else if(key == '0'){
+                  tone(buzzer, 0.65925 * 1000, 200);
+                 }
             if(key == code[i]&& i<sizeof(code)){            //if the char typed is correct a and i increments to verify the next caracter
                  a++;                                              //Now I think maybe I should have use only a or i ... too lazy to test it -_-'
                  i++;
-                 }
-            else
-                a--;                                               //if the character typed is wrong a decrements and cannot equal the size of code []
+                 
+            }
+            else{
+                //tone(buzzer, key * 10, 200);
+                a--;}                                            //if the character typed is wrong a decrements and cannot equal the size of code []
             }
             }
 }
 
 void OpenDoor(){             //Lock opening function open for 10s
-  lcd.clear();
-  lcd.print("Nice");//With a message printed
-  digitalWrite(redPin, LOW); //Included lights, cause why not
-  digitalWrite(greenPin, HIGH);
-  servo1.write(-80);
-  delay(10000);
-  servo1.write(80);
-  digitalWrite(greenPin, LOW); 
-  digitalWrite(redPin, HIGH);}
+  servo1.write(-40);
+  tone(buzzer, 100, 1000);
+  delay(700);
+  tone(buzzer, 90, 1000); 
+  delay(700);
+  tone(buzzer, 80, 1000);
+  delay(700);
+  tone(buzzer, 70, 1000);
+  delay(700);
+  tone(buzzer, 60, 1000);
+  delay(700);
+  tone(buzzer, 50, 1000);
+  delay(700);
+  tone(buzzer, 40, 1000);
+  delay(700);
+  tone(buzzer, 30, 1000);
+  delay(700);
+  tone(buzzer, 20, 1000);
+  delay(700);
+  tone(buzzer, 10, 1000);
+  delay(700);
+  
+  
+  //noTone(buzzer); //Stop buzzer
+  servo1.write(40);}
 
 void loop() {
   // put your main code here, to run repeatedly:
     char key = keypad.getKey();               //Constantly waiting for a key to be pressed
-    digitalWrite(redPin, HIGH);
-    if(key == '*'){                      // * to open the lock
-            lcd.clear();
-            lcd.setCursor(0,0);
-            lcd.print("Enter code");            //Message to show
+    if(key == '*'){                      // * to open the lock            //Message to show
             GetCode();                          //Getting code function
                   if(a==sizeof(code)){           //The GetCode function assign a value to a (it's correct when it has the size of the code array)
                   OpenDoor();                   //Open lock function if code is correct
-                  }else{
-                  lcd.clear();               
-                  lcd.print("Wrong");          //Message to print when the code is wrong
+                  }else{          //Message to print when the code is wrong
                   }
-            delay(2000);
-            lcd.clear();
-            lcd.print("Standby");             //Return to standby mode it's the message do display when waiting
+            delay(2000);             //Return to standby mode it's the message do display when waiting
         }
 }        
 
